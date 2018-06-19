d0e8b3cac200ac2c2c4199fb1d22cd5eeb3d7a9b
fdrevet <florian.drevet@free.fr>
2018-06-19 18:51:09 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,8 +7,8 @@ keytool \
       -v \
       -trustcacerts \
       -alias 0 \
-      -file <(openssl x509 -in /etc/openssl/input.pem \
-      -keystore /etc/openssl/output.bks \
+      -file <(openssl x509 -in /etc/openssl/$PEM_FILE \
+      -keystore /etc/openssl/$BKS_FILE \
       -storetype BKS \
       -provider org.bouncycastle.jce.provider.BouncyCastleProvider \
       -providerpath /src/bcprov.jar \
